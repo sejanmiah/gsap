5d51b9daeb25bd55c0e83eeb6268bb150bde5166
sejanmiah <sejankhan931@gmail.com>
2025-05-08 00:05:35 +0400

    OnMouse Move Done

--- src/component/GsapClickAnimation.jsx
+++ src/component/GsapClickAnimation.jsx
@@ -7,7 +7,8 @@ const GsapClickAnimation = () => {
     const [rotating, setRotating] = useState(0);
     const boxRef = useRef(null);
 
-    const random = () => gsap.utils.random(-500,500,150)
+    const randomX = () => gsap.utils.random(-500,500,150)
+    const randomRotate = () => gsap.utils.random(-360,360,10)
 
     useGSAP(() => {
         gsap.to(boxRef.current, {
@@ -18,14 +19,26 @@ const GsapClickAnimation = () => {
         })
     },[xMove,rotating])
 
+    const btnRef = useRef(null);
+
+    const [btnMove, SetBtnMove] = useState(0);
+    const randomBtnMove = gsap.utils.random(-350,350,140)
+
+const moveBtn = () => {
+    gsap.to(btnRef.current, {
+        x: btnMove,
+    })
+}
+
+
 
     return (
         <div>
             <h1 className='text-5xl font-bold text-center mt-5'>Gsap Animation By Click</h1>
             <div className='flex flex-col min-h-screen justify-center items-center gap-5'>
                 <button onClick={() => {
-                    setXMove(random)
-                     setRotating(random)
+                    setXMove(randomX)
+                     setRotating(randomRotate)
                 } } className='btn bg-gradient-to-t from-sky-500 to-indigo-500 text-white px-5 py-2 rounded'
                 >
                     Click the animate box
@@ -34,7 +47,16 @@ const GsapClickAnimation = () => {
                     ref={boxRef}
                     className='box w-[120px] h-[120px] rounded-[8px] bg-gradient-to-br from-purple-500 to-pink-500'
                 ></div>
+
+
+                
+                <div onMouseMove={()=> {
+                    moveBtn(SetBtnMove(randomBtnMove))
+                }}>
+                    <button className='btn absolute'  ref={btnRef}>Hate You</button>
+                </div>
             </div>
+
         </div>
     );
 };
